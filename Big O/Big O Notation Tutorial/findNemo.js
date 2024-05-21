d82687c332e15047af9298d7ecb67e569e3aa220
chandanguptabsb <chandanguptabsb@gmail.com>
2024-05-21 06:56:30 +0000

    Added Big O/Big O Notation Tutorial/

---
+++ Big O/Big O Notation Tutorial/findNemo.js
@@ -0,0 +1,11 @@
+let nemo = ["nemo"];
+
+function findNemo(arr){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==="nemo"){
+            console.log("Found NEMO!")
+        }
+    }
+}
+
+findNemo(nemo);
